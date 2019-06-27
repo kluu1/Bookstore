e8d742f450f561c2b22c32507313bb50375b2319
Kevin Luu <kluu@epictech.cm>
2019-06-26 22:44:07 -0400

    add getProduct route

--- controllers/product.js
+++ controllers/product.js
@@ -4,8 +4,25 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandlers');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: 'Product not found'
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.getProduct = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
-  let form = new formidable.IncomingForm();
+  const form = new formidable.IncomingForm();
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
@@ -13,9 +30,31 @@ exports.create = (req, res) => {
         error: 'Image could not be uploaded'
       });
     }
-    let product = new Product(fields);
+    // validate all fields are present
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      });
+    }
+
+    const product = new Product(fields);
 
     if (files.photo) {
+      // restrict photo size to less than 1mb
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb in size'
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
@@ -23,7 +62,7 @@ exports.create = (req, res) => {
     product.save((err, result) => {
       if (err) {
         return res.status(400).json({
-          error: errorHandler(error)
+          error: errorHandler(err)
         });
       }
       res.json(result);
