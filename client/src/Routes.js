aa5efa78f5faa2217de4a9232f98afcafc99028d
Kevin Luu <kluu@epictech.cm>
2019-07-08 22:44:36 -0400

    added private route for user dashboard

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './core/Home';
 import Signup from './user/Signup';
 import Login from './user/Login';
+import PrivateRoute from './auth/PrivateRoutes';
+import Dashboard from './user/UserDashboard';
 
 
 const Routes = () => {
@@ -12,6 +14,7 @@ const Routes = () => {
         <Route path='/' exact component={Home} />
         <Route path='/signup' exact component={Signup} />
         <Route path='/login' exact component={Login} />
+        <PrivateRoute path="/dashboard" exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
